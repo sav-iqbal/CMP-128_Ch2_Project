f4324b2b9dbff5a8c4b616dd6ca134f2a95db6e8
Savaas Iqbal <122247798+sav-iqbal@users.noreply.github.com>
2023-09-11 11:13:39 -0400

    Add files via upload

---
+++ Chapter 2 Project.java
@@ -0,0 +1,48 @@
+import java.util.Scanner; 
+public class Chapter 2 Project {
+
+   public static void main (String [] args) 
+      {
+      // Personal Information 
+   /* Write	a	program	segment	that	displays	the	following	information,	each	on	a	separate	line:
+       - Your	name
+       - Your	address,	with	city,	state,	and	zip
+       - Your	telephone	number   
+       - Name	of	college
+       - Although	these	items	should	be	displayed	on	separate	output	lines,	use	only	a	single
+       println statement	in	your	program.
+   */
+   string name = "Savaas"; 
+   string city = "xxxxxx"; 
+   string cellNumber = "973-xxx-xxxx"; 
+   string college = "Cornell University"; 
+   System.out.println("My name is " + name + ". \n" + "My city is " + city + ". \n"  + "My phone number is " + cellNumber + ". \n" + "My college is " + college + ". \n"); 
+   
+   //String Manipulator 
+   /*
+   Write	a	program	segment	that	asks	the	user	to	enter	the	name	of	his	or	her	favorite	city.	
+   Use	a	String	variable	to	store	the	input.		The	program	should	display	the	following:
+   The	number	of	characters	in	the	city	name
+   - The	name	of	the	city	in	all	uppercase	letters
+   - The	name	of	the	city	in	all	lowercase	letters.
+   - The	first	character	in	the	name	of	the	city
+   */
+   
+   //Stock Transaction Program 
+  /*  Write	a	program	segment	that	details	a	stock	purchase.
+   Last	month	Joe	purchased	some	stock	in	Acme	Software,	Inc.		Here	are	the	details	of	the	
+   purchase:
+   - The	number	of	shares	that	Joe	purchased	was	1,000
+   - When	Joe	purchased	the	stock,	he	paid	$32.87	per	share
+   - Joe	paid	his	stockbroker	a	commission	that	amounted	to	2%	of	the	amount	he	
+   received	for	the	stock
+   Two	weeks	later	Joe	sold	the	stock.	Here	are	the	details	of	the	sale:
+   - The	number	of	shares	that	Joe	sold	was	1,000
+   - He	sold	the	stock	for	$33.92	per	share
+   - He	paid	his	stockbroker	another	commission	that	amounted	to	2%	of	the	amount	he	
+   received	for	the	stock */
+   
+      }
+
+
+}
